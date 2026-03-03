db28cf16de98f36558224bcebfac6bf6137febcd
hngyen <hnguyen.05@outlook.com>
2026-03-03 21:57:21 +1100

    debugging cors issue with status code 200

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["railvision-frontend.vercel.app", "http://localhost:5173/"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
